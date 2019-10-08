8cd468bba30102703aa41675333d0d91b4fedba0
b-vera <bastian.vera.p@usach.cl>
2019-10-08 11:07:45 -0300

    CHG: first header format

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,45 @@
 <template>
-    <div>Este es el header</div>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a
+                role="button"
+                class="navbar-burger burger"
+                aria-label="menu"
+                aria-expanded="false"
+                data-target="navbarBasicExample"
+            >
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <a class="button">
+                            <div class="dropdown is-hoverable is-right">
+                                <div class="dropdown-trigger">
+                                    <span>Idioma</span>
+                                    <span class="icon is-small">
+                                        <i class="fas fa-angle-down" aria-hidden="true"></i>
+                                    </span>
+                                </div>
+                                <div class="dropdown-menu" id="dropdown-menu" role="menu">
+                                    <div class="dropdown-content">
+                                        <a href="#" class="dropdown-item">Español</a>
+                                        <a href="#" class="dropdown-item">Inglés</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                        <a class="button is-primary">
+                            <strong>Ingresa</strong>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
 </template>
