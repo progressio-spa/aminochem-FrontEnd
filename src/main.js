805466be2b7a1b0f6963f42d89a01c04045788c0
b-vera <bastian.vera.p@usach.cl>
2019-10-08 11:08:57 -0300

    CHG: fontawesome added

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render(h) { return h(App); },
+  render(h) {
+    return h(App);
+  },
 }).$mount('#app');
