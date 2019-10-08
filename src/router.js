e47fb7ac26f6df3beb0196bfa5e73964b94d27d9
b-vera <bastian.vera.p@usach.cl>
2019-10-08 11:08:30 -0300

    CHG: all the import added

--- src/router.js
+++ src/router.js
@@ -1,6 +1,12 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import About from './views/About.vue';
+import Contact from './views/Contact.vue';
+import DistributionNetwork from './views/DistributionNetwork.vue';
+import Login from './views/Login.vue';
+import Products from './views/Products.vue';
+import TechnicalSection from './views/TechnicalSection.vue';
 
 Vue.use(Router);
 
@@ -14,12 +20,32 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component() {
-        return import(/* webpackChunkName: "about" */ './views/About.vue');
-      },
+      component: About,
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contact,
+    },
+    {
+      path: '/distributionNetwork',
+      name: 'DistributionNetwork',
+      component: DistributionNetwork,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
+    {
+      path: '/products',
+      name: 'products',
+      component: Products,
+    },
+    {
+      path: '/technicalSection',
+      name: 'technicalSection',
+      component: TechnicalSection,
     },
   ],
 });
