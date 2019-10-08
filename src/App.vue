cf41b38a8764ca03399eb37180c95576714ba1e7
b-vera <bastian.vera.p@usach.cl>
2019-10-08 11:09:58 -0300

    CHG: footer added to this view

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,34 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <div id="nav">
+            <!-- <router-link to="/">Home</router-link>|
+            <router-link to="/about">About</router-link>-->
+        </div>
+        <Header></Header>
+        <router-view />
+        <Footer></Footer>
     </div>
-    <router-view/>
-  </div>
 </template>
+
+<script>
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+    name: 'home',
+    components: {
+        Header,
+        Footer,
+    },
+}
+</script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 </style>
