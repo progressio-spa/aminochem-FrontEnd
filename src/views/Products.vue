67baf43e53e3fa9c10cc9fbb093ce9f582d809a5
b-vera <bastian.vera.p@usach.cl>
2019-11-06 22:23:02 -0300

    CHG: NavBar added to page

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,3 +1,18 @@
 <template>
-    <div>Products</div>
+    <div class="products">
+        <Navbar></Navbar>
+        <div>Products</div>
+    </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue';
+
+export default {
+  name: 'home',
+  components: {
+    Navbar,
+  },
+};
+</script>
--- src/views/TechnicalSection.vue
+++ src/views/TechnicalSection.vue
@@ -1,3 +1,18 @@
 <template>
-    <div>technical section</div>
+    <div class="technical">
+        <Navbar></Navbar>
+        <div>technical section</div>
+    </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue';
+
+export default {
+  name: 'home',
+  components: {
+    Navbar,
+  },
+};
+</script>
