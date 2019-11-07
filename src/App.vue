7a58018c13c872e719732527d77564d30f4b165a
b-vera <bastian.vera.p@usach.cl>
2019-11-06 22:21:41 -0300

    CHG: ubuntu font added

--- src/App.vue
+++ src/App.vue
@@ -11,24 +11,26 @@
 </template>
 
 <script>
-import Header from '@/components/Header.vue'
-import Footer from '@/components/Footer.vue'
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
 
 export default {
-    name: 'home',
-    components: {
-        Header,
-        Footer,
-    },
-}
+  name: 'home',
+  components: {
+    Header,
+    Footer,
+  },
+};
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Ubuntu:300,400&display=swap');
+
 #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    font-family: 'Ubuntu', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #2c3e50;
+    color: #585858;
 }
 </style>
