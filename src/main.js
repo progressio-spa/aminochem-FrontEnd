ba36f2ae66cec15fa4987ee23e5838d1107f3fa0
b-vera <bastian.vera.p@usach.cl>
2019-11-06 22:22:14 -0300

    CHG: VUEI18n added

--- src/main.js
+++ src/main.js
@@ -4,12 +4,26 @@ import router from './router';
 import store from './store';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+import VueI18n from 'vue-i18n';
+import { languages, defaultLocale } from './langs/index.js';
+
+Vue.use(VueI18n);
+
+const messages = Object.assign(languages);
 
 Vue.config.productionTip = false;
+
+// Create VueI18n instance with options
+const i18n = new VueI18n({
+  locale: defaultLocale, // set locale
+  fallbackLocale: 'es',
+  messages, // set locale messages
+});
 
 new Vue({
   router,
   store,
+  i18n,
   render(h) {
     return h(App);
   },
