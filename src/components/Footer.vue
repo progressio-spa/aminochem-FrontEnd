a0a7ea83b584107f984a0e44852b157745923510
b-vera <bastian.vera.p@usach.cl>
2019-11-11 22:15:25 -0300

    CHG: logo and buttons are added

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,93 @@
 <template>
-    <footer class="footer">
-        <div class="content has-text-centered">
-            <p>
-                <strong>Bulma</strong> by
-                <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
-                <a
-                    href="http://opensource.org/licenses/mit-license.php"
-                >MIT</a>. The website content
-                is licensed
-                <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
-            </p>
-        </div>
-    </footer>
+    <div class="footerMain">
+        <footer class="footer">
+            <div class="content">
+                <div class="columns is-desktop is-vcentered ">
+                    <div class="column">
+                        <div class="footer-item">
+                            <router-link to="/">
+                                <img src="@/assets/logo-aminochem.png" alt="Aminochem" />
+                            </router-link>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <a class="item">
+                            <router-link to="/about">
+                                {{ $t('Navbar.aboutUs') }}
+                            </router-link>
+                        </a>
+                    </div>
+                    <div class="column">
+                        <a class="item">
+                            <router-link to="/products">
+                                {{ $t('Navbar.products') }}
+                            </router-link>
+                        </a>
+                    </div>
+                    <div class="column">
+                        <a class="item">
+                            <router-link to="/distributionNetwork">
+                                {{ $t('Navbar.network') }}
+                            </router-link>
+                        </a>
+                    </div>
+                    <div class="column">
+                        <a class="item">
+                            <router-link to="/technicalSection">
+                                {{ $t('Navbar.technical') }}
+                            </router-link>
+                        </a>
+                    </div>
+                    <div class="column">
+                        <div class="item">
+                            <router-link to="/contact">
+                                {{ $t('Navbar.contact') }}
+                            </router-link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <section class="hero has-background-grey-light">
+            <div class="hero-footer">
+                <div class="last-content">
+                    <div class="paragraph">
+                        <p>
+                            Edificio BHBC Av. Bello Horizonte 869 / Of 304 - Rancagua
+                        </p>
+                    </div>
+                    <div class="watermark">
+                        <p>
+                            ©copyright 2019
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 </template>
+
+<style scoped>
+.footer {
+    background-color: #e5e5e5;
+    display: flex;
+    justify-content: center;
+}
+.footer-item img {
+    max-height: 5rem;
+}
+.content {
+    width: 75%;
+}
+a {
+    color: #585858;
+}
+a:hover {
+    color: #fff;
+}
+.last-content {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+}
+</style>
