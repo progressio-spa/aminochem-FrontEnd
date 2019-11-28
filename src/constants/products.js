bd569054243959a715dc2b4a727e7daad39e8dd2
b-vera <bastian.vera.p@usach.cl>
2019-11-27 22:47:35 -0300

    ADD: products info here

---
+++ src/constants/products.js
@@ -0,0 +1,39 @@
+export const fertilizers = [
+    {
+        name: 'Aminoterra',
+    },
+    {
+        name: 'Aminoterra Green',
+    },
+    {
+        name: 'Aminoterra Pus Zinc',
+    },
+    {
+        name: 'Aminoterra Plus Zinc (Argentina)',
+    },
+    {
+        name: 'Bioamino-L Active Praderas',
+    },
+    {
+        name: 'Aminosoil Premium Polvo',
+    },
+    {
+        name: 'Bioil-S (Enhance)',
+    },
+]
+
+export const amendments = [
+    {
+        name: 'Bioamino-L',
+    },
+]
+export const rootBiostimulant = [
+    {
+        name: 'Aminoroot',
+    },
+]
+export const insecticide = [
+    {
+        name: 'SalmonOil',
+    },
+]
