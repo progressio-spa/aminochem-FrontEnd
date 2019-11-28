3f5e9f30e51bf26faa057d7c18ad822d53372031
b-vera <bastian.vera.p@usach.cl>
2019-11-27 22:48:33 -0300

    ADD: views for products

---
+++ src/views/products/Amendments.vue
@@ -0,0 +1,180 @@
+<template>
+    <div class="amendments">
+        <Navbar></Navbar>
+        <section class="hero is-primary is-medium has-background">
+            <img
+                alt="Grass"
+                class="hero-background is-transparent"
+                src="@/assets/Products/hoja.jpg"
+            />
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title" id="main-title">
+                        {{ $t('Products.amendments.title') }}
+                    </h1>
+                </div>
+            </div>
+        </section>
+        <section class="hero is-white is-fullheight">
+            <div class="hero-body">
+                <div class="container">
+                    <div class="columns">
+                        <div class="column is-half">
+                            <div class="content">
+                                <div class="title">
+                                    {{ products[0].name }}
+                                </div>
+                                <p align="left">
+                                    {{ $t('Products.amendments.description1') }}
+                                </p>
+                                <p align="left">
+                                    {{ $t('Products.amendments.description2') }}
+                                </p>
+                                <p align="left">
+                                    {{ $t('Products.amendments.description3') }}
+                                </p>
+                                <p align="left">
+                                    {{ $t('Products.amendments.description4') }}
+                                </p>
+                                <p align="left">
+                                    {{ $t('Products.amendments.description5') }}
+                                </p>
+                                <p align="left">
+                                    {{ $t('Products.amendments.description6') }}
+                                </p>
+                                <p align="left">
+                                    {{ $t('Products.amendments.description7') }}
+                                </p>
+                                <p align="left">
+                                    {{ $t('Products.amendments.description8') }}
+                                </p>
+                            </div>
+                        </div>
+                        <div class="column is-half">
+                            <figure class="image is-5by4">
+                                <img src="https://bulma.io/images/placeholders/600x480.png" />
+                            </figure>
+                        </div>
+                    </div>
+                    <br />
+                    <br />
+                    <nav class="level is-desktop">
+                        <div class="level-item">
+                            <button class="button is-medium">
+                                <span class="icon">
+                                    <i class="fas fa-tag"></i>
+                                </span>
+                                <span>
+                                    {{ $t('Products.buttons.label') }}
+                                </span>
+                            </button>
+                        </div>
+                        <div class="level-item">
+                            <button class="button is-medium">
+                                <span class="icon">
+                                    <i class="fas fa-file-alt"></i>
+                                </span>
+                                <span>
+                                    {{ $t('Products.buttons.datasheet') }}
+                                </span>
+                            </button>
+                        </div>
+                        <div class="level-item">
+                            <button class="button is-medium">
+                                <span class="icon">
+                                    <i class="fas fa-exclamation-triangle"></i>
+                                </span>
+                                <span>
+                                    {{ $t('Products.buttons.safetysheet') }}
+                                </span>
+                            </button>
+                        </div>
+                        <div class="level-item">
+                            <button class="button is-medium">
+                                <span class="icon">
+                                    <i class="fas fa-info-circle"></i>
+                                </span>
+                                <span>
+                                    {{ $t('Products.buttons.brochure') }}
+                                </span>
+                            </button>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </section>
+        <br /><br />
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+
+// vue-function-api imports
+import { onMounted, value, computed } from 'vue-function-api'
+
+// Import Fertilizer products
+import { amendments } from '../../constants/products'
+
+export default {
+    name: 'home',
+    components: {
+        Navbar,
+    },
+    setup() {
+        const products = amendments
+        return {
+            products,
+        }
+    },
+}
+</script>
+
+<style scoped>
+.hero.has-background {
+    position: relative;
+    overflow: hidden;
+}
+.hero-background {
+    position: absolute;
+    object-fit: cover;
+    object-position: center center;
+    width: 100%;
+    height: 100%;
+}
+
+#main-title {
+    display: flex;
+    font-size: 3rem;
+}
+
+.card-header {
+    background-color: gray;
+}
+
+.hero.is-white .title,
+p {
+    color: #575757;
+}
+
+.content > .title {
+    display: flex;
+    size: 2.5rem;
+}
+
+.button {
+    background-color: #ff715b;
+    color: white;
+    padding: 0 3vw 0 3vw;
+}
+
+@media (max-width: 767px) {
+    .button {
+        width: 90vw;
+    }
+    #main-title {
+        font-size: 2rem;
+    }
+}
+</style>
---
+++ src/views/products/Fertilizers.vue
@@ -0,0 +1,229 @@
+<template>
+    <div class="fertilizers">
+        <Navbar></Navbar>
+        <section class="hero is-primary is-medium has-background">
+            <img
+                alt="Grass"
+                class="hero-background is-transparent"
+                src="@/assets/Products/hoja.jpg"
+            />
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title" id="main-title">
+                        {{ $t('Products.fertilizers.title') }}
+                    </h1>
+                </div>
+            </div>
+        </section>
+        <section class="hero is-white is-fullheight">
+            <div class="hero-body">
+                <div class="container">
+                    <div class="columns is-desktop">
+                        <div class="column is-one-third">
+                            <div class="card">
+                                <div class="card-image">
+                                    <figure class="image is-4by3">
+                                        <img
+                                            src="https://bulma.io/images/placeholders/480x480.png"
+                                            alt="Placeholder image"
+                                        />
+                                    </figure>
+                                </div>
+                                <div class="card-header">
+                                    <div class="card-header-title is-centered">
+                                        <div class="title is-4">
+                                            {{ products[0].name }}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <br />
+                        </div>
+                        <div class="column is-one-third">
+                            <div class="card">
+                                <div class="card-image">
+                                    <figure class="image is-4by3">
+                                        <img
+                                            src="https://bulma.io/images/placeholders/480x480.png"
+                                            alt="Placeholder image"
+                                        />
+                                    </figure>
+                                </div>
+                                <div class="card-header">
+                                    <div class="card-header-title is-4 is-centered">
+                                        <div class="title is-4">
+                                            {{ products[1].name }}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <br />
+                        </div>
+                        <div class="column is-one-third">
+                            <div class="card">
+                                <div class="card-image">
+                                    <figure class="image is-4by3">
+                                        <img
+                                            src="https://bulma.io/images/placeholders/480x480.png"
+                                            alt="Placeholder image"
+                                        />
+                                    </figure>
+                                </div>
+                                <div class="card-header">
+                                    <div class="card-header-title is-centered">
+                                        <div class="title is-4">
+                                            {{ products[2].name }}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <br />
+                        </div>
+                    </div>
+                    <div class="columns is-desktop">
+                        <div class="column is-one-third">
+                            <div class="card">
+                                <div class="card-image">
+                                    <figure class="image is-4by3">
+                                        <img
+                                            src="https://bulma.io/images/placeholders/480x480.png"
+                                            alt="Placeholder image"
+                                        />
+                                    </figure>
+                                </div>
+                                <div class="card-header">
+                                    <div class="card-header-title is-centered">
+                                        <div class="title is-4">
+                                            {{ products[3].name }}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <br />
+                        </div>
+                        <div class="column is-one-third">
+                            <div class="card">
+                                <div class="card-image">
+                                    <figure class="image is-4by3">
+                                        <img
+                                            src="https://bulma.io/images/placeholders/480x480.png"
+                                            alt="Placeholder image"
+                                        />
+                                    </figure>
+                                </div>
+                                <div class="card-header">
+                                    <div class="card-header-title is-centered">
+                                        <div class="title is-4">
+                                            {{ products[4].name }}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <br />
+                        </div>
+                        <div class="column is-one-third">
+                            <div class="card">
+                                <div class="card-image">
+                                    <figure class="image is-4by3">
+                                        <img
+                                            src="https://bulma.io/images/placeholders/480x480.png"
+                                            alt="Placeholder image"
+                                        />
+                                    </figure>
+                                </div>
+                                <div class="card-header">
+                                    <div class="card-header-title is-centered">
+                                        <div class="title is-4">
+                                            {{ products[5].name }}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <br />
+                        </div>
+                    </div>
+                    <div class="columns is-desktop">
+                        <div class="column is-one-third is-offset-one-third">
+                            <div class="card">
+                                <div class="card-image">
+                                    <figure class="image is-4by3">
+                                        <img
+                                            src="https://bulma.io/images/placeholders/480x480.png"
+                                            alt="Placeholder image"
+                                        />
+                                    </figure>
+                                </div>
+                                <div class="card-header">
+                                    <div class="card-header-title is-centered">
+                                        <div class="title is-4">
+                                            {{ products[6].name }}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <br />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <br /><br />
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+
+// vue-function-api imports
+import { onMounted, value, computed } from 'vue-function-api'
+
+// Import Fertilizer products
+import { fertilizers } from '../../constants/products'
+
+export default {
+    name: 'home',
+    components: {
+        Navbar,
+    },
+    setup() {
+        const products = fertilizers
+        return {
+            products,
+        }
+    },
+}
+</script>
+
+<style scoped>
+.hero.has-background {
+    position: relative;
+    overflow: hidden;
+}
+.hero-background {
+    position: absolute;
+    object-fit: cover;
+    object-position: center center;
+    width: 100%;
+    height: 100%;
+}
+
+#main-title {
+    display: flex;
+    font-size: 3rem;
+}
+
+.card-header {
+    background-color: gray;
+}
+
+.hero.is-white .title {
+    color: white;
+}
+
+@media (max-width: 767px) {
+    #main-title {
+        font-size: 2rem;
+    }
+}
+</style>
