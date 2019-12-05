b3b0ddf8a9ce6d585be69ac78ac14a226200e8cd
b-vera <bastian.vera.p@usach.cl>
2019-12-04 23:07:50 -0300

    CHG: product format update

--- src/constants/products.js
+++ src/constants/products.js
@@ -1,6 +1,8 @@
 export const fertilizers = {
     aminoterra: {
         name: 'Aminoterra',
+        description:
+            'Es un poderoso fertilizante y bioestimulante líquido, obtenido a partir de proteínas de origen marino (Salmón), hidrolizadas enzimáticamente en condiciones controladas con alta tecnología. Este avanzado proceso, permite obtener un producto estable, rico en aminoácidos libres y péptidos de bajo peso molecular, favoreciendo la absorción vía foliar y radicular, lo que permite a las plantas construir sus propias proteínas y tejidos de forma rapida, con gran ahorro de energía, especialmente en ambientes con estrés de tipo nutricional, térmico, hídrico, luminoso ó salino.',
     },
     'aminoterra-green': {
         name: 'Aminoterra Green',
