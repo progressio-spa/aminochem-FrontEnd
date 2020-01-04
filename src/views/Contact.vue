13bfd17f2bb9106f8c165e2fe28ddc48520f9c65
b-vera <bastian.vera.p@usach.cl>
2020-01-04 19:50:31 -0300

    CHG: maps added to view and responsiveness fixed

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -9,39 +9,41 @@
             />
             <div class="hero-body">
                 <div class="container">
-                    <h1 class="title is-1" id="main-title">
-                        {{ $t('Contact.title') }}
-                    </h1>
+                    <h1 class="title is-1" id="main-title">{{ $t('Contact.title') }}</h1>
                 </div>
             </div>
         </section>
-        <br /><br />
-        <section class="hero is-light is-medium">
+        <section class="hero is-medium">
             <div class="hero-body">
-                <div class="container">
-                    <h1 class="title">
-                        mapa :)
-                    </h1>
+                <div class="container" id="maps">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3300.6699177581363!2d-70.72903213671648!3d-34.18035601296084!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9663434fdc75591b%3A0x2b22977ac88c2f81!2sCalle%20Bello%20Horizonte%20869%2C%20Rancagua%2C%20O&#39;Higgins!5e0!3m2!1ses-419!2scl!4v1578177395787!5m2!1ses-419!2scl"
+                        width="1000"
+                        height="500"
+                        frameborder="0"
+                        style="border:0;"
+                        allowfullscreen
+                    ></iframe>
                 </div>
             </div>
         </section>
-        <br /><br />
         <ContactBox></ContactBox>
+        <br />
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Navbar from '@/components/Navbar.vue';
-import ContactBox from '@/components/ContactBox.vue';
+import Navbar from '@/components/Navbar.vue'
+import ContactBox from '@/components/ContactBox.vue'
 
 export default {
-  name: 'home',
-  components: {
-    Navbar,
-    ContactBox,
-  },
-};
+    name: 'home',
+    components: {
+        Navbar,
+        ContactBox,
+    },
+}
 </script>
 
 <style scoped>
@@ -57,6 +59,14 @@ export default {
     height: 100%;
 }
 
+#maps {
+    height: 500px;
+}
+
+iframe {
+    width: 100%;
+    height: 500px;
+}
 #main-title {
     display: flex;
 }
