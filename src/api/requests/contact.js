ccda1d40bb37bd9c43c026916be6dbf7ed4dd44b
Sebastian Salazar <sebastian.salazar.v@usach.cl>
2020-01-06 17:14:51 -0300

    TODO: Change port to deployed one
    Signed-off-by: Sebastian Salazar <sebastian.salazar.v@usach.cl>

--- src/api/requests/contact.js
+++ src/api/requests/contact.js
@@ -2,7 +2,7 @@ import request from '../index';
 
 function contact(data) {
   return request({
-    url: 'http://157.245.134.21:8081/contact',
+    url: 'http://157.245.134.21:8083/contact',
     method: 'post',
     headers: {
       'Content-Type': 'application/json; charset=UTF-8',
