d48142c6a2bfd592477086472f82b2ef626f6a93
Sebastian Salazar <sebastian.salazar.v@usach.cl>
2020-01-09 00:33:25 -0300

    FIX: Fixed port for axios requestd
    Signed-off-by: Sebastian Salazar <sebastian.salazar.v@usach.cl>

--- src/api/requests/authorization.js
+++ src/api/requests/authorization.js
@@ -2,7 +2,7 @@ import request from '../index';
 
 export function getAccessToken(data) {
   return request({
-    url: 'http://157.245.134.21:8083/login',
+    url: 'http://157.245.134.21:8081/login',
     method: 'post',
     headers: {
       'Content-Type': 'application/json; charset=UTF-8',
@@ -13,7 +13,7 @@ export function getAccessToken(data) {
 
 export function registerUser(data) {
   return request({
-    url: 'http://157.245.134.21:8083/register',
+    url: 'http://157.245.134.21:8081/register',
     method: 'post',
     headers: {
       'Content-Type': 'application/json; charset=UTF-8',
@@ -24,7 +24,7 @@ export function registerUser(data) {
 
 export function forgotPassword(data) {
   return request({
-    url: 'http://157.245.134.21:8083/forgotPassword',
+    url: 'http://157.245.134.21:8081/forgotPassword',
     method: 'post',
     headers: {
       'Content-Type': 'application/json; charset=UTF-8',
