d3565989f64061b05c2e783e71968904b35e21a5
b-vera <bastian.vera.p@usach.cl>
2020-03-01 20:31:50 -0300

    ADD: temporal Technincal information

---
+++ src/constants/TemporalTechnical.js
@@ -0,0 +1,20 @@
+export const iplusd = {
+    demo1: {
+        title:
+            'Use of soil/root bio-stimulant Bioamino-L in Mandarin, Lemon, Orange and Avocado Crops',
+        imgURL: '../static/Temporal/demo1.jpg',
+        subtitle: 'Use of soil/root bio-stimulant',
+    },
+    demo2: {
+        title:
+            'Use of Bioamino-L as a seed treatment complement in soybean crop and  its effect on the yield components',
+        imgURL: '../static/Temporal/demo2.jpg',
+        subtitle: 'Use of Bioamino-L',
+    },
+    demo3: {
+        title:
+            'Evaluación de Biol-S (Aceite de salmón) en mezcla con el herbicida Flumioxanzin para el control de Amaranthus hybridus L.',
+        imgURL: '../static/Temporal/demo3.jpg',
+        subtitle: 'Evaluación de Biol-S',
+    },
+}
