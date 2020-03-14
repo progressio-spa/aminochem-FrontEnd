3eea8fe521295bb1ee932cb20aa1eb059889dc42
b-vera <bastian.vera.p@usach.cl>
2020-03-13 23:03:22 -0300

    FIX: new text in products

--- src/constants/products.js
+++ src/constants/products.js
@@ -3,58 +3,51 @@ export const fertilizers = {
         name: 'Aminoterra',
         description:
             'Es un poderoso fertilizante y bioestimulante líquido, obtenido a partir de proteínas de origen marino (salmón), hidrolizadas enzimáticamente en condiciones controladas con alta tecnología. Este avanzado proceso, permite obtener un producto estable, rico en aminoácidos libres de péptidos de bajo peso molecular, favoreciendo la absorción vía foliar y radicular, lo que permite a las plantas construir sus propias proteínas y tejidos de forma rapida, y con gran ahorro de energía, especialmente en ambientes con estrés de tipo nutricional, térmico, hídrico, luminoso ó salino.',
-        subdescription1:
-            'Proporciona una nutrición directa con aminoácidos (Aporte de nitrógeno metabolizado con un gran ahorro energético para las plantas).',
-        subdescription2:
-            'Fortalece y recupera las plantas sometidas a situaciones de estrés (disminuyendo pérdidas de potencial productivo).',
-        subdescription3: 'Induce formación de plantas más sanas, resistentes y vigorosas.',
+        subdescription1: 'Nutrición directa con Aminoácidos.',
+        subdescription2: 'Corrige y previene los efectos del Estrés.',
+        subdescription3:
+            'Activa procesos clave en la planta gracias a la presencia de los 20 aminoácidos esenciales en su composición.',
+        subdescription4: 'Mejores plantas, mayor potencial, mejor resultado.',
         imgURL: '../static/product-logo/Variantes-productosColor-08.png',
-        label: 'src/assets/Downloads/Aminoterra/label.pdf',
-        specs: '',
-        security: '',
+        certificated: '',
     },
     'aminoterra-green': {
         name: 'Aminoterra Green',
-        description:
-            'Es un poderoso fertilizante y bioestimulante líquido, obtenido a partir de proteínas de origen marino (salmón), hidrolizadas enzimaticamente en condiciones controladas con alta tecnologia. Este avanzado proceso, permite obtener un producto estable, rico en aminoácidos libres de péptidos de bajo peso molecular, favoreciendo la absorción vía foliar y radicular, lo que permite a las plantas construir sus propias proteínas y tejidos de forma rápida, y con gran ahorro de energía, especialnente en ambientes con estrés de tipo nutricional, térmico, hídrico, luminoso ó salino.',
-        subdescription1:
-            'Proporciona una nutrición directa con aminoácidos (Aporte de nitrógeno metabolizado con un gran ahorro energético para las plantas).',
-        subdescription2:
-            'Fortalece y recupera las plantas sometidas a situaciones de estrés (disminuyendo pérdidas de potencial productivo).',
-        subdescription3: 'Induce formación de plantas más sanas, resistentes y vigorosas.',
+        description: 'Todos los beneficios de Aminoterra, en su versión orgánica.',
+        subdescription1: 'Nutrición directa con Aminoácidos.',
+        subdescription2: 'Corrige y previene los efectos del Estrés.',
+        subdescription3:
+            'Activa procesos clave en la planta gracias a la presencia de los 20 aminoácidos esenciales en su composición.',
+        subdescription4: 'Mejores plantas, mayor potencial, mejor resultado.',
         imgURL: '../static/product-logo/Variantes-productosColor-10.png',
-        label: '',
-        specs: '',
-        security: '',
+        certificated: '../static/certificate/all.png',
     },
     'aminoterra-plus': {
         name: 'Aminoterra Plus Zinc',
         description:
             'Es un poderoso fertilizante y bioestimulante líquido, obtenido a partir de proteínas de origen marino (salmón), hidrolizadas enzimaticamente en condiciones controladas con alta tecnologia. Este avanzado proceso, permite obtener un producto estable, rico en aminoácidos libres de péptidos de bajo peso molecular, favoreciendo la absorción vía foliar y radicular, lo que permite a las plantas construir sus propias proteínas y tejidos de forma rápida, y con gran ahorro de energía, especialnente en ambientes con estrés de tipo nutricional, térmico, hídrico, luminoso ó salino.',
-        subdescription1:
-            'Proporciona una nutrición directa con aminoácidos (Aporte de nitrógeno metabolizado con un gran ahorro energético para las plantas).',
-        subdescription2:
-            'Fortalece y recupera las plantas sometidas a situaciones de estrés (disminuyendo pérdidas de potencial productivo).',
-        subdescription3: 'Induce formación de plantas más sanas, resistentes y vigorosas.',
+        subdescription1: 'Nutrición directa con Aminoácidos.',
+        subdescription2: 'Corrige y previene los efectos del Estrés.',
+        subdescription3:
+            'Activa procesos clave en la planta gracias a la presencia de los 20 aminoácidos esenciales en su composición.',
+        subdescription4: 'Enriquecido con Zn al 2%.',
+        subdescription5: 'Mejores plantas, mayor potencial, mejor resultado.',
         imgURL: '../static/product-logo/Variantes-productosColor-03.png',
-        label: '',
-        specs: '',
-        security: '',
+        certificated: '',
     },
 
     Aminosoil: {
         name: 'Aminosoil Premium Polvo',
         description:
-            'Es un fertilizante de estrablecimiento y enmienda de suelo, rico en materia orgánica y aminoácidos de muy alta calidad biológica, más macro y microelementos. Producto de última generación, amigable con el medio ambiente, de fácil asimilación por las plantas, diseñado para el establecimiento, mejoramiento de huertos debilitados de frutales mayores y menores, plantaciones forestales, cultivos anuales, hortalizas, bulbos, semilleros, cultivos ornamentales, viveros frutales y forestales.',
+            'Es un fertilizante de establecimiento y enmienda de suelo, rico en Nitrógeno Orgánico, materia orgánica y aminoácidos de muy alta calidad biológica, más macro y microelementos. Producto de última generación, amigable con el medio ambiente, de fácil asimilación por las plantas, diseñado para el establecimiento, mejoramiento de huertos debilitados de frutales mayores y menores, plantaciones forestales, cultivos anuales, hortalizas, bulbos, semilleros, cultivos ornamentales, viveros frutales y forestales.',
         subdescription1:
-            'Proporciona una nutrición eficaz a los cultivos, potenciando su sistema radicular e inmunologico (concentrado en lisina), promueve la vida orgánica y microorganismos del suelo.',
-        subdescription2:
-            'AMINOSOIL PREMIUM, es un producto nacional de alta calidad para la fertilización moderna de los cultivos, ha sido testeado en distintas especies vegetales y muestra una respuesta sobresaliente en el establecimiento y recuperación de las mismas.',
-        subdescription3: '',
+            'AMINOSOIL PREMIUM, es un producto Chileno de alta calidad para la fertilización moderna de los cultivos, ha sido testeado en distintas especies vegetales, mostrando una respuesta sobresaliente en el establecimiento y recuperación de las mismas.',
+        subdescription2: 'Fuente de Nitrógeno Orgánico.',
+        subdescription3: 'Rico en Materia Orgánica lábil.',
+        subdescription4: 'Producto apto para fertirriego.',
+        subdescription5: 'Apto para Agricultura Orgánica.',
         imgURL: '../static/product-logo/Variantes-productosColor-05.png',
-        label: '',
-        specs: '',
-        security: '',
+        certificated: '',
     },
     aminoroot: {
         name: 'Aminoroot',
@@ -66,9 +59,7 @@ export const fertilizers = {
             'Mejora la vitalidad de las plantas provocando una mejor captación y absorción de nutrientes y agua.',
         subdescription3: '',
         imgURL: '../static/product-logo/Variantes-productosColor-04.png',
-        label: '',
-        specs: '',
-        security: '',
+        certificated: '',
     },
 }
 
@@ -83,22 +74,19 @@ export const naturalOil = {
             'Producto de su origen, Bioil-S (Enhance) también posee un interesante contenido de micronutrientes lo que contribuye a una nutrición exitosa en todo tipo de cultivos, hortalizas y frutales.',
         subdescription3: '',
         imgURL: '../static/product-logo/Variantes-productosColor-07.png',
-        label: '',
-        specs: '',
-        security: '',
+        certificated: '../static/certificate/ecocert.png',
     },
     SalmonOil: {
         name: 'SalmonOil',
         description:
-            'Es un poderoso fertilizante y bioestimulante líquido, obtenido a partir de proteínas de origen marino (salmón), hidrolizadas enzimaticamente en condiciones controladas con alta tecnologia. Este avanzado proceso, permite obtener un producto estable, rico en aminoácidos libres de péptidos de bajo peso molecular, favoreciendo la absorción vía foliar y radicular, lo que permite a las plantas construir sus propias proteínas y tejidos de forma rápida, y con gran ahorro de energía, especialnente en ambientes con estrés de tipo nutricional, térmico, hídrico, luminoso ó salino.',
+            'Las plantas que disponen de proteínas y ácidos grasos Omega en forma constante durante su ciclo, no solo mejoran su resistencia y adaptación a todo tipo de ambientes, sino también aumentan la capacidad de síntesis de hormonas naturales de crecimiento con el consecuente aumento en potencial productivo y calidad de todos sus tejidos (raíces, hojas, tallos, flores y frutos).',
         subdescription1:
-            'Las plantas que disponen de proteínas y acidos grasos Omega en forma constante durante su ciclo, no solo mejoran su resistencia y adaptación a todo tipo de ambientes, sino también aumentan la capacidad de síntesis de hormonas naturales de crecimientom con el consecuente aumento en potencial productivo y calidad de todos sus tejidos (raíces, hojas, tallos, flores y frutos).',
-        subdescription2:
-            'Producto de su origen, SalmonOil también posee un interesante contenido de micronutrientes lo que contribuye a una nutrición exitosa en todo tipo de cultivos, hortalizas y frutales.',
-        subdescription3: '',
+            'Producto de su origen, Bioil-S (Enhance) también posee un interesante contenido de micronutrientes lo que contribuye a una nutrición exitosa en todo tipo de cultivos, hortalizas y frutales.',
+        subdescription2: 'Aceite natural de origen marino, rico en Omega 3,6 y 9.',
+        subdescription3: 'Bioestimulante y coadyuvante',
+        subdescription4: 'Acción Biopesticida como insecticida y fungistático',
+        subdescription5: 'Certificación Orgánica.',
         imgURL: '../static/product-logo/Variantes-productosColor-11.png',
-        label: '',
-        specs: '',
-        security: '',
+        certificated: '../static/certificate/ecocert.png',
     },
 }
