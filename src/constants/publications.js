4667c5cde17673109977f04c4c498085ca796610
b-vera <bastian.vera.p@usach.cl>
2020-03-24 18:23:08 -0300

    ADD: new publications

---
+++ src/constants/publications.js
@@ -0,0 +1,14 @@
+export const publication = {
+    demo4: {
+        title: 'Bioamino-L:    Vida de la rizósfera probando texto de relleno',
+        imgURL: require('@/assets/Temporal/demo4.jpg'),
+        subtitle: '',
+        pdfName: 'demo4',
+    },
+    demo5: {
+        title: 'Bioamino-L:   Un efectivo activador biológico de suelos',
+        imgURL: require('@/assets/Temporal/demo5.jpg'),
+        subtitle: '',
+        pdfName: 'demo5',
+    },
+}
