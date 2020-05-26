239eafb4bac43108b8bcc2fa40b40527c9eca3f7
b-vera <bastian.vera.p@usach.cl>
2020-05-26 19:07:01 -0400

    CHG: new name for export const

--- src/constants/TemporalTechnical.js
+++ src/constants/TemporalTechnical.js
@@ -1,23 +1,22 @@
-export const iplusd = {
-    demo1: {
-        title:
-            'Use of soil/root bio-stimulant Bioamino-L in Mandarin, Lemon, Orange and Avocado Crops',
-        imgURL: require('@/assets/Temporal/demo1.jpg'),
-        subtitle: 'Use of soil/root bio-stimulant',
-        pdfName: 'demo1',
-    },
-    demo2: {
-        title:
-            'Use of Bioamino-L as a seed treatment complement in soybean crop and  its effect on the yield components',
-        imgURL: require('@/assets/Temporal/demo2.jpg'),
-        subtitle: 'Use of Bioamino-L',
-        pdfName: 'demo2',
-    },
-    demo3: {
-        title:
-            'Evaluación de Biol-S (Aceite de salmón) en mezcla con el herbicida Flumioxanzin para el control de Amaranthus hybridus L.',
-        imgURL: require('@/assets/Temporal/demo3.jpg'),
-        subtitle: 'Evaluación de Biol-S',
-        pdfName: 'demo3',
-    },
-}
+export const ipd = {
+  demo1: {
+    title: "Use of soil/root bio-stimulant Bioamino-L in Mandarin, Lemon, Orange and Avocado Crops",
+    imgURL: require("@/assets/Temporal/demo1.jpg"),
+    subtitle: "Use of soil/root bio-stimulant",
+    pdfName: "demo1"
+  },
+  demo2: {
+    title:
+      "Use of Bioamino-L as a seed treatment complement in soybean crop and  its effect on the yield components",
+    imgURL: require("@/assets/Temporal/demo2.jpg"),
+    subtitle: "Use of Bioamino-L",
+    pdfName: "demo2"
+  },
+  demo3: {
+    title:
+      "Evaluación de Biol-S (Aceite de salmón) en mezcla con el herbicida Flumioxanzin para el control de Amaranthus hybridus L.",
+    imgURL: require("@/assets/Temporal/demo3.jpg"),
+    subtitle: "Evaluación de Biol-S",
+    pdfName: "demo3"
+  }
+};
